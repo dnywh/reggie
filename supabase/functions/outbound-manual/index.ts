605d119f1f6eff5595c7cafaa082f1bc63051669
Danny White <3104761+dnywh@users.noreply.github.com>
2025-10-30 16:18:15 +1000

    simple manual email

--- supabase/functions/outbound-manual/index.ts
+++ supabase/functions/outbound-manual/index.ts
@@ -2,38 +2,17 @@ import "jsr:@supabase/functions-js/edge-runtime.d.ts";
 import { Resend } from "npm:resend";
 const resend = new Resend(Deno.env.get("RESEND_API_KEY"));
 const FROM_EMAIL = `Reggie <${Deno.env.get("REGGIE_EMAIL")}>`;
-const TEST_EMAIL = Deno.env.get("TEST_EMAIL");
-const REGGIE_URL = Deno.env.get("REGGIE_URL");
 
 Deno.serve(async () => {
   const recipient = ""; // Falsy if empty string
-  const to = recipient ? recipient : TEST_EMAIL;
-
-  const includePreferencesLink = true;
 
   try {
-    const subject = "Hey there";
+    const subject = "";
     const html = `
-      Hello there!
-      Here is a list of things:
-      <ul>
-        <li>Thing 1</li>
-        <li>Thing 2</li>
-        <li>Thing 3</li>
-      </ul>
+<p>Text goes here</p>
 
-      Thanks for testing!
-      Reggie
-
-      ${
-      includePreferencesLink
-        ? `
-      ---
-      P.S. am I emailing too much? Too little? You can <a href="${REGGIE_URL}/preferences?email=${to}">edit your preferences</a> at any time.
-      `
-        : ""
-    }
-      `;
+<p>Reggie</p>
+`;
 
     await resend.emails.send({
       from: FROM_EMAIL,
